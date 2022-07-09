3add5884a67edfdc51d825b57ea5e90986e1cad1
h0gan1ee <lihaojun2003@outlook.com>
2022-07-09 20:53:22 +0800

    feat: why no error messages?

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import {defineConfig} from 'vite'
-import {svelte} from '@sveltejs/vite-plugin-svelte'
-import postcss from "postcss";
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  css: {
-    postcss
-  }
-})
+  base: "./",
+});

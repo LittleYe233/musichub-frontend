00182dbd32faf4f6d554a5da5ba68ad346f99e67
h0gan1ee <lihaojun2003@outlook.com>
2022-07-09 13:12:31 +0800

    feat: UI

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import {defineConfig} from 'vite'
+import {svelte} from '@sveltejs/vite-plugin-svelte'
 import postcss from "postcss";
 
 // https://vitejs.dev/config/

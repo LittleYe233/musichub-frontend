222d8bbacda2518393c07c3d0d7439009b991dea
LittleYe233 <30514318+LittleYe233@users.noreply.github.com>
2022-08-08 20:11:10 +0800

    feat(config): add project config `musichub.config.example.js`

---
+++ musichub.config.example.js
@@ -0,0 +1,11 @@
+/** @type {import('./src/types/config').RawProjectConfig} */
+export default {
+  client: {
+    host: '0.0.0.0',
+    port: 3000,
+    strictPort: false,
+  },
+  server: {
+    root: 'http://localhost:8083',
+  },
+};

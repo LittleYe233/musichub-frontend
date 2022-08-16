69aa0e57388dcf468f4f8ecf32e16997f4995d58
LittleYe233 <30514318+LittleYe233@users.noreply.github.com>
2022-08-16 23:47:02 +0800

    feat(lib.api): add `parseSongs()` to back-end API

--- src/types/api/backend.d.ts
+++ src/types/api/backend.d.ts
@@ -2,13 +2,12 @@ import type BackendAPI from 'src/lib/api/backend';
 import type { SongPiece } from '../config';
 
 export default interface IBackendAPI {
-  /**
-   * Root URL of API entrypoints.
-   */
   root: string;
 
   public static parseEndpoint(endpoint: string, options?: { root: string }): string;
   parseEndpoint(endpoint: string): string;
 
   getSongs(): Promise<SongPiece[] | Response>;
+
+  parseSongs(songs: SongPiece[]): SongPiece[];
 }
